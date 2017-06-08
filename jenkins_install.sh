fa8b116ab1be472f203cf190ea7d19808fb4ce75
ohyoungjooung2 <wnapdlf05@gmail.com>
2017-06-08 19:57:49 +0900

    jenkins_install.sh modified sudo su -c option

--- jenkins_install.sh
+++ jenkins_install.sh
@@ -1,21 +1,28 @@
 #!/usr/bin/env bash
+JAVA_DIR=/opt/java
 
 java_check(){
   which java
-  if [[ $? == "0" ]]
+  c=`find $JAVA_DIR -name "java"` 
+  if [[ $c ]]
   then
      echo "java installed"
-     java -version
   else
      echo "You must install java to run jenkins"
+     echo "Do you want to install oracle java?"
+     read ANSWER
+     if [[ $ANSWER=="y" ]]
+     then
+       sudo bash oracle_java_download_install.sh 8
+     fi
   fi
 }
 
 install_jenkins(){
   if [[ -e /usr/bin/apt ]]
   then
-   wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-   echo "deb https://pkg.jenkins.io/debian-stable binary/" >> /etc/apt/sources.list
+   sudo wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
+   sudo su -c "echo 'deb https://pkg.jenkins.io/debian-stable binary/' >> /etc/apt/sources.list"
    sudo apt-get update -y
    sudo apt-get install jenkins -y
   elif [[ -e /bin/yum ]]
@@ -31,6 +38,7 @@ install_jenkins(){
 
 check_jenkins(){
   ps -ef | grep -i jenkins
+  sudo systemctl status jenkins.service
 }
 
 java_check
